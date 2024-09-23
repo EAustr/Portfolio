6c2c68ef42da3117796f24fcdea25ae038c0b97b
Ernests <austrins.ernests@gmail.com>
2024-09-23 23:07:30 +0300

    Removed some spaces and added comments in js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 window.onload = function() {
-
+    // Atrodam hamburger pogu
     const menu_btn = document.querySelector('.hamburger');
+    // Atrodam mobilo navigācijas izvēlni
     const mobile_menu = document.querySelector('.mobile-nav');
 
+    // Pievienojam klikšķa notikumu klausītāju hamburger pogai
     menu_btn.addEventListener('click', function() {
+        // Pārslēdzam 'is-active' klasi hamburger pogai
         menu_btn.classList.toggle('is-active');
+        // Pārslēdzam 'is-active' klasi mobilajai navigācijas izvēlnei
         mobile_menu.classList.toggle('is-active');
     });
 }
